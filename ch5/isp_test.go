1cc148e3b1d1c6cecf98a82fe16d89022f9745d3
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-11-07 10:35:32 +0900

    IsEnabledメソッドのテストをテーブルドリブンにした

--- ch5/isp_test.go
+++ ch5/isp_test.go
@@ -2,15 +2,47 @@ package ch5
 
 import "testing"
 
-func TestResProviderIsEnabled(t *testing.T) {
+func TestIsEnabled(t *testing.T) {
 
-	resProvider := ResProvider{value: true}
+	testCases := []struct {
+		description string
+		provider    BooleanProvider
+		expected    bool
+	}{
+		{
+			description: "ResProvider with value true",
+			provider:    ResProvider{value: true},
+			expected:    true,
+		}, {
+			description: "ResProvider with value false",
+			provider:    ResProvider{value: false},
+			expected:    false,
+		}, {
+			description: "SoundProvider with value true",
+			provider:    SoundProvider{value: true},
+			expected:    true,
+		}, {
+			description: "SoundProvider with value false",
+			provider:    SoundProvider{value: false},
+			expected:    false,
+		}, {
+			description: "VibrationProvider with value true",
+			provider:    VibrationProvider{value: true},
+			expected:    true,
+		}, {
+			description: "VibrationProvider with value false",
+			provider:    VibrationProvider{value: false},
+			expected:    false,
+		},
+	}
 
-	result := resProvider.IsEnabled()
-
-	expected := true
-	if result != expected {
-		t.Errorf("ResProvider.IsEnabled() = %v; want  %v", result, expected)
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			result := tc.provider.IsEnabled()
+			if result != tc.expected {
+				t.Errorf("%s: got %v, want %v", tc.description, result, tc.expected)
+			}
+		})
 	}
 
 }
