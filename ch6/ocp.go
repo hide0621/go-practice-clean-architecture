47e57e4825c8d97341d45d9026009eea63d7791d
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-11-13 09:54:41 +0900

    オープンクローズドの原則の違反パターン

---
+++ ch6/ocp.go
@@ -0,0 +1,31 @@
+package ch6
+
+import "math"
+
+type Shape struct {
+	Type   string
+	Width  float64
+	Height float64
+	Radius float64
+}
+
+func NewShape(t string) *Shape {
+	return &Shape{Type: t}
+}
+
+func (s *Shape) Area() float64 {
+	if s.Type == "rectangle" {
+		return s.RectangleArea()
+	} else if s.Type == "circle" {
+		return s.CircleArea()
+	}
+	return 0
+}
+
+func (s *Shape) RectangleArea() float64 {
+	return s.Width * s.Height
+}
+
+func (s *Shape) CircleArea() float64 {
+	return math.Pi * math.Pow(s.Radius, 2)
+}
